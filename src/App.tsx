a7a8372b99226f1a358afd6c7dd21b45ee0946a3
Sergio Borges <sergio.borgesjunior@everis.nttdata.com>
2022-06-22 15:52:51 -0300

    refatorando atualização de evento e adicionando função de dragFinish no calendario

--- src/App.tsx
+++ src/App.tsx
@@ -35,13 +35,13 @@ function App() {
 
     // setEventos([...eventos])
   }
-  const alterarStatusEvento = (id: number) => {
+  // const alterarStatusEvento = (id: number) => {
     // const evento = eventos.find(evento => evento.id === id)
     // if (evento) {
     //   evento.completo = !evento.completo
     // }
     // setEventos([...eventos])
-  }
+  // }
   // const deletarEvento = (id: number) => {
   //   setEventos([...eventos.filter(evento => evento.id !== id)])
   // }
@@ -65,7 +65,7 @@ function App() {
           </Card>
           <hr />
           <Card>
-            <ListaDeEventos aoFiltroAplicado={aplicarFiltro} aoAlterarStatus={alterarStatusEvento} />
+            <ListaDeEventos aoFiltroAplicado={aplicarFiltro} />
           </Card>
         </div>
         <div className={style.Coluna}>
